431720cee1ff3d7c9382439488bae66f49cd9a43
Stam2000 <sopemmanuel@gmail.com>
2024-12-12 15:39:36 +0100

    up

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "https://full-stack-estate-main-k9cj.onrender.com/api",
+  baseURL: import.meta.env.VITE_SERVER_URL,
   withCredentials: true,
 });
 
