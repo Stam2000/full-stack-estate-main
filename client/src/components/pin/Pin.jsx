29fa9dc37ad6bdcbeabab8a4ac078ded640ddb9d
Stam2000 <sopemmanuel@gmail.com>
2024-12-12 20:45:23 +0100

    Update Logo  and

--- client/src/components/pin/Pin.jsx
+++ client/src/components/pin/Pin.jsx
@@ -1,10 +1,20 @@
 import { Marker, Popup } from "react-leaflet";
 import "./pin.scss";
 import { Link } from "react-router-dom";
+import L from 'leaflet';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
+
 
 function Pin({ item }) {
   return (
-    <Marker position={[item.latitude, item.longitude]}>
+    <Marker  position={[item.latitude, item.longitude]}>
       <Popup>
         <div className="popupContainer">
           <div className="textContainer">
